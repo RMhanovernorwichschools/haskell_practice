76ec2bfda8527072359d6bb81e063862125f21e6
Ramachtt24 <ramachtt24@hanovernorwichschools.org>
2019-02-07 18:33:17 +0000

    Not much. Just an update.

--- UNT.hs
+++ UNT.hs
@@ -3,13 +3,19 @@ phrase x = if x==True then "Yeah." else "I don't think so..."
 idSpaces l = [x | x<- [0..(length l -1)], l!!x==' ']
 wordify l = [[ l!!x | x <- [(([0]++idSpaces l )!!a)..((idSpaces l ++[(length l - 1)])!!a)], l!!x/=' ', not(elem (l!!x) (['['..'`']++[':'..'@']++".,?{}+-~!"))] | a<- [0..length (idSpaces l)] ]
 
-check w = if (lowercase w=="not" || lowercase w=="no") then "False" else 
-        if (elem (lowercase w) ["school", "schools", "homework", "HW", "class", "classwork", "schoolwork"]) then "School" else
-        if (elem (lowercase w) ["food", "breakfast", "snack", "lunch", "dinner", "meal"]) then "Eating" else
+check w = if (lowercase w=="not" || lowercase w=="no") then "Negation" else 
+        if (elem (lowercase w) ["school", "schools", "homework", "HW", "class", "classwork", "schoolwork"]) then "TOP_School" else
+        if (elem (lowercase w) ["food", "breakfast", "snack", "lunch", "dinner", "meal"]) then "TOP_Eating" else
+        if (elem (lowercase w) ["joy", "joyous", "great", "awesome", "finally"]) then "TNM_Celebrate" else
         "none"
-flavor l = if (elem "School" [check x | x<-(wordify l)] ) then "Try to have fun at class, maybe." else
-        if (elem "Eating" [check x | x<-(wordify l)] ) then "Don't feel too hungry. Food awaits." else
-        "I dunno."
+flavor l = (if elem "TNM_Celebrate" [check x | x<-(wordify l)] then "Yeah! " else "") ++
+        --These are the topic-specific statements which are probably the meat, for this function.
+        (if (elem "TOP_School" [check x | x<-(wordify l)]) && (elem "TOP_School" [check x | x<-(wordify l)]) then "I wonder if the cafeteria food is any good" else
+        if (elem "TOP_School" [check x | x<-(wordify l)] ) then "Try to have fun at class, maybe" else
+        if (elem "TOP_Eating" [check x | x<-(wordify l)] ) then "Don't feel too hungry. Food awaits" else
+        "I dunno") ++
+        --This is additional comment stuff to do with miscellaneous
+        (if elem "Negation" [check x | x<-(wordify l)] then ", but I don't know. I'm probably wrong." else ".")
 
 lowify l = [ ['a'..'z']!!n | n<- [0..25], l==['A'..'Z']!!n]
 lowercase x = [ if x `elem` ['A'..'Z'] then ((lowify x)!!0) else x | x<- x]
